e081d4dfc5a2e38dcaea5d6d964c3856de191598
Daniel <101201029+Gabuchev@users.noreply.github.com>
2023-02-12 16:40:25 +0300

    Update app.py

--- Flask/app.py
+++ Flask/app.py
@@ -4,9 +4,10 @@ from flask import Flask, render_template, url_for, request,send_from_directory
 import pandas as pd  
 import csv
 from ClassCSV import DataCSV
-import json
 
 app = Flask(__name__,static_url_path='') 
+
+#app.run()
 menu = [{"name":"ПРОЕКТ", "url": "base"},
 {"name":"РЕКОМЕНДАЦИИ", "url": "result"},
 ]
@@ -36,7 +37,7 @@ def result():
     metro = pd.read_csv(r'C:\Users\Daniel\project university\Csv_to_html\ClassSave\metro.csv',encoding='utf-8')
     #Обработка входных данных
     if request.method == 'POST':
-
+       
         Cost = request.form['Cost']
         if Cost == '' :
             Cost = dataСlean["cost(₽)"].median()               
@@ -61,31 +62,46 @@ def result():
         else:
             CenterDistance = metro[metro['metro_name'] == MetroName]["center_distance/m"].values.astype("int")
             CenterDistance=str(CenterDistance).strip('[]') 
-      
+       
 #################################################################################################################################  
             
         if request.form['button'] == 'all':
 
-            DataCSV(Cost,Square, Distance, Floor, Floors, Description, MetroName,CenterDistance).RecAll()      
+            DataCSV(Cost,Square, Distance, Floor, Floors, Description, MetroName,CenterDistance).RecAll()   
+           #DataCSV(Cost,Square, Distance, Floor, Description, MetroName,CenterDistance).RecAll()   
 
         if request.form['button'] == 'cost':
 
             DataCSV(Cost,Square, Distance, Floor, Floors, Description, MetroName,CenterDistance).RecCost()
-
+           #DataCSV(Cost,Square, Distance, Floor, Description, MetroName,CenterDistance).RecCost()  
 
         if request.form['button'] == 'description':
 
             DataCSV(Cost,Square, Distance, Floor, Floors, Description, MetroName,CenterDistance).RecDescription()
+           # DataCSV(Cost,Square, Distance, Floor, Description, MetroName,CenterDistance).RecDescription()  
+
          
     #################################################################################################################################    
+
+
+        
+     #plt.tight_layout()
+
     recom_df = pd.read_json(r'C:\Users\Daniel\project university\Csv_to_html\ClassSave\recom_df.json')
     dataResult = recom_df.to_dict('records') 
-    output1 = recom_df.head(5)
-    output = round(output1['Cost'].mean())
+    #output1 = recom_df.head(5)
+    #output = round(output1['Cost'].mean())
+    Diagnostics_df = pd.read_csv(r'C:\Users\Daniel\project university\Csv_to_html\ClassSave\Diagnostics_df.csv')
+    output_Cost = Diagnostics_df["Predict_Cost"][0].round(decimals=5)
+    output_Cost1 = Diagnostics_df["RMSE"][0].round(decimals=5)
+    output_Cost2 = Diagnostics_df["MAE"][0].round(decimals=5)
+    output_Cost3 = Diagnostics_df["MAPE"][0].round(decimals=5)
+    output_Cost4 = Diagnostics_df["r2"][0].round(decimals=5)
+
     reader = csv.reader(open(r"C:\Users\Daniel\project university\Csv_to_html\ClassSave\DFTableInput.csv",encoding='utf-8'))
-    return render_template('result.html',title ="Рекомендации", tableD = dataResult, csv = reader, menu = menu, output=output)
+    return render_template('result.html',title ="Рекомендации", tableD = dataResult, csv = reader, menu = menu, output=output_Cost,output1=output_Cost1,
+    output2=output_Cost2,output3=output_Cost3,output4=output_Cost4,user_image="saved_figure.png")
     #################################################################################################################################    
-    
 
 if __name__ == "__main__":
      app.run(debug=True)
